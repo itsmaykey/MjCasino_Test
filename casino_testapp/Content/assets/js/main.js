88a0019c6a0f351f9a366c8fbdbcec38f6495434
Mike Irvin Jelan Ladja <mijelan0905@gmail.com>
2024-05-03 11:05:22 +0800

    day 5

--- casino_testapp/Content/assets/js/main.js
+++ casino_testapp/Content/assets/js/main.js
@@ -43,9 +43,6 @@
    * Sidebar toggle
    */
 
-  
-
-
   if (select('.toggle-sidebar-btn')) {
     on('click', '.toggle-sidebar-btn', function(e) {
       select('body').classList.toggle('toggle-sidebar')
