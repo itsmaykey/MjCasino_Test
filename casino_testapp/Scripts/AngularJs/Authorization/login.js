76fdf4a8b324d54ebaad3d09b4c22a3f2738a6f1
Mike Irvin Jelan Ladja <mijelan0905@gmail.com>
2024-05-09 15:11:00 +0800

    login-update1

--- casino_testapp/Scripts/AngularJs/Authorization/login.js
+++ casino_testapp/Scripts/AngularJs/Authorization/login.js
@@ -1,10 +1,3 @@
-﻿var app = angular.module("loginApp", []);
-app.controller('loginCtrlr', ['$scope', '$http', '$filter', function (s, h, f) {
-
-
+﻿app.controller('loginCtrlr', ['$scope', '$http', '$filter', function (s, h, f) {
     console.log("Hello");
-    s.clickni = function () {
-        console.log("Hello");
-    }
-
 }])
