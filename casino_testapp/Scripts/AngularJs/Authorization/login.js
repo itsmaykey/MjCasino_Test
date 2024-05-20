0199175325afdf8edb2810b7a47a8876a8d0da97
Mike Irvin Jelan Ladja <mijelan0905@gmail.com>
2024-05-20 08:20:24 +0800

    footerbackground

--- casino_testapp/Scripts/AngularJs/Authorization/login.js
+++ casino_testapp/Scripts/AngularJs/Authorization/login.js
@@ -2,12 +2,16 @@
 app.controller('loginCtrlr', ['$scope', '$http', '$filter', '$window', function (s, h, f, w) {
     s.username;
     s.password;
-
+    s.uname
     s.login = function () {
+       
         console.log(s.username, s.password);
         h.post("../api/Authorization/Login?username=" + s.username +"&password=" + s.password).success(function (data) {
             console.log(data);
             w.sessionStorage.setItem("user", JSON.stringify(data["data"]));
+
+  
+         
             console.log(w.sessionStorage.getItem("user"));
         });
     }
